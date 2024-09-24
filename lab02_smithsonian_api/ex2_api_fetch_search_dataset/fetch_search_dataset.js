f39cf167e996829f818ff469c2c0e67d07eb1ae5
Nour Al Safadi <nour.safadi7@icloud.com>
2024-09-23 22:00:22 -0400

    cleans the data structre being fetched in addObject() function

--- lab02_smithsonian_api/ex2_api_fetch_search_dataset/fetch_search_dataset.js
+++ lab02_smithsonian_api/ex2_api_fetch_search_dataset/fetch_search_dataset.js
@@ -1,27 +1,9 @@
-// Smithsonian API example code
-// check API documentation for search here: http://edan.si.edu/openaccess/apidocs/#api-search-search
-// Using this data set https://collections.si.edu/search/results.htm?q=Flowers&view=grid&fq=data_source%3A%22Cooper+Hewitt%2C+Smithsonian+Design+Museum%22&fq=online_media_type%3A%22Images%22&media.CC0=true&fq=object_type:%22Embroidery+%28visual+works%29%22
-
-
-/* 
-To restrict queries for only records with associated media:
-q=online_visual_material:true
-*/
-
-
 import {apiKey} from './ApiKey.js';
-// search base URL
+
 const searchBaseURL = "https://api.si.edu/openaccess/api/v1.0/search";
 
-// constructing the initial search query
-// const search =  'mask AND unit_code:"FSG"';
-
-/* Here the rawCount is +15k which is not matching the result I get at https://collections.si.edu/search/ */
-// const search =  `Portrait AND unit_code:"NPG" AND online_media_type:"Images"`;
-
-/* Here the rawCount is 466, which very close to the resulte at https://collections.si.edu/search/  14,467 */
+/* to check results  ––––––––>  https://collections.si.edu/search/  */
 const search =  `unit_code:"NMNHENTO" AND online_visual_material:true`;
-
 
 // array that we will write into
 let myArray = [];
@@ -85,60 +67,23 @@ function fetchAllData(url) {
 // create your own array with just the data you need
 function addObject(objectData) {  
   
-  // we've encountered that some places have data others don't
-  let currentPlace = "";
-  if(objectData.content.indexedStructured.place) {
-    currentPlace = objectData.content.indexedStructured.place[0];
-  } 
-
-  myArray.push({
+//undefined check for coutnry 
+let itemCountry = objectData.content.indexedStructured.geoLocation ? objectData.content.indexedStructured.geoLocation[0].L2.content : "not specified";
+let itemName = objectData.title || "Not Specified";
+let taxClass = objectData.content.indexedStructured.tax_class ? objectData.content.indexedStructured.tax_class[0] : "Not Specified";
+let taxFamily = objectData.content.indexedStructured.tax_family ? objectData.content.indexedStructured.tax_family[0] : "Not Specified";
+let taxKingdom = objectData.content.indexedStructured.tax_kingdom ? objectData.content.indexedStructured.tax_kingdom[0] : "Not Specified";
+let topic = objectData.content.indexedStructured.topic ? objectData.content.indexedStructured.topic[0] : "Not Specified";
+  
+myArray.push({
     A_id: objectData.id,
-    name: objectData.title,
-    class: objectData.content.indexedStructured.tax_class[0],
-    family: objectData.content.indexedStructured.tax_family[0],
-    tax_kingdom: objectData.content.indexedStructured.tax_kingdom[0],
-    Topic: objectData.content.indexedStructured.topic[0],
-    Country: objectData.content.indexedStructured.geoLocation[0].L2.content,
-    // Image: objectData.content.descriptiveNonRepeating.online_media.media[0].thumbnail,
-    place: currentPlace
+    name: itemName,
+    class: taxClass,
+    family: taxFamily,
+    tax_kingdom: taxKingdom,
+    Topic: topic,
+    Country: itemCountry,
   })
 }
 
-
 fetchSearchData(search);
-
-
-//---------------------------UNIT CODES------------------------------
-// ACAH: Archives Center, National Museum of American History
-// ACM: Anacostia Community Museum
-// CFCHFOLKLIFE: Smithsonian Center for Folklife and Cultural Heritage
-// CHNDM: Cooper-Hewitt, National Design Museum
-// FBR: Smithsonian Field Book Project
-// FSA: Freer Gallery of Art and Arthur M. Sackler Gallery Archives
-// FSG: Freer Gallery of Art and Arthur M. Sackler Gallery
-// HAC: Smithsonian Gardens
-// HMSG: Hirshhorn Museum and Sculpture Garden
-// HSFA: Human Studies Film Archives
-// NAA: National Anthropological Archives
-// NASM: National Air and Space Museum
-// NMAAHC: National Museum of African American History and Culture
-// NMAfA: Smithsonian National Museum of African Art
-// NMAH: Smithsonian National Museum of American History
-// NMAI: National Museum of the American Indian
-// NMNHANTHRO: NMNH - Anthropology Dept.
-// NMNHBIRDS: NMNH - Vertebrate Zoology - Birds Division
-// NMNHBOTANY: NMNH - Botany Dept.
-// NMNHEDUCATION: NMNH - Education & Outreach
-// NMNHENTO: NMNH - Entomology Dept.
-// NMNHFISHES: NMNH - Vertebrate Zoology - Fishes Division
-// NMNHHERPS: NMNH - Vertebrate Zoology - Herpetology Division
-// NMNHINV: NMNH - Invertebrate Zoology Dept.
-// NMNHMAMMALS: NMNH - Vertebrate Zoology - Mammals Division
-// NMNHMINSCI: NMNH - Mineral Sciences Dept.
-// NMNHPALEO: NMNH - Paleobiology Dept.
-// NPG: National Portrait Gallery
-// NPM: National Postal Museum
-// SAAM: Smithsonian American Art Museum
-// SI: Smithsonian Institution, Digitization Program Office
-// SIA: Smithsonian Institution Archives
-// SIL: Smithsonian Libraries
