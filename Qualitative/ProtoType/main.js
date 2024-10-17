0051847a656f8b0344e7ecde291e3b1c5f269e30
Nour Al Safadi <nour.safadi7@icloud.com>
2024-10-17 16:44:25 -0400

    structures css

--- Qualitative/ProtoType/main.js
+++ Qualitative/ProtoType/main.js
@@ -1,7 +1,3 @@
-/*
-RiTa.js reference:
-https://rednoise.org/rita/reference/index.php
-*/
 
 let myJson = "/dataTarget.json";
 let historical_context = [
@@ -21,9 +17,7 @@ let politics = [
   "revolution", "independence", "self-determination", "emancipation", "insurrection", "defiance", "overthrow", "abolition", 
   "protest", "civil disobedience", "struggle","leader", "ruler", "dictator", "emperor", "king", "laws", "policy", "imperial", "authority", "tyranny", 
   "command", "sovereignty", "government", "control", "dominion", "oppression", "governance", "rule", "regime", 
-  "bureaucracy", "empowerment", "subjugation", "leader", "ruler", "dictator", "emperor", "king", "laws", "policy", "imperial", "authority", "tyranny", 
-  "command", "sovereignty", "government", "control", "dominion", "oppression", "governance", "rule", "regime", 
-  "bureaucracy", "empowerment", "subjugation"
+  "bureaucracy", "empowerment", "subjugation",
 ]
 
 let social_and_cultural_ideologies = [
@@ -50,7 +44,7 @@ async function fetchData(url) {
       throw new Error(`Network response was not ok (status ${response.status})`);
     }
     const data = await response.json(myJson);   
-    return data.slice(0, 1002);
+    return data.slice(0, 200);
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error; 
@@ -58,7 +52,7 @@ async function fetchData(url) {
 }
 
 function showLoading() {
-  const app = d3.select('#appContainer');
+  const app = d3.select('.main');
   app.append('div')
     .attr('class', 'loading')
     .text('...LOADING...');
@@ -70,186 +64,188 @@ function hideLoading() {
 
 showLoading();
 
+const historyColor = "rgb(203, 96, 64)";
+const politicColor = "rgb(37, 113, 128)";
+
 const dataPromise = fetchData(myJson)
-    .then(data => {
-    hideLoading();
-    console.log("this is My data", data);
-    // Loop through data and return only the name property
-    const names = data.map(item => item.name);
-    const type = data.map(item => item.objectType);
-    console.log("Names:",  names);
-    console.log("type:",  type);
+  .then(data => {
+  hideLoading();
+  console.log("this is My data", data);
 
-    // Create a new div and display the names
-    const app = d3.select('#appContainer');
-    const mainContainer = app.append('div').attr('class', 'main-container');
-
+  // Fragment use. to reduce reflows and improve performance
+  const fragment = document.createDocumentFragment();
   
-    // Use DocumentFragment to minimize reflows and repaints
-    const fragment = document.createDocumentFragment();
-    let subContainer = document.createElement('div');
+  // select the main div
+  const app = d3.select('.main-viz');
+  const mainContainer = app.append('div').attr('class', 'main-viz-container');
+
+  let subContainer = document.createElement('div');
+  subContainer.className = 'sub-container';
+  
+  // Loop through data and return only the name property
+  const names = data.map(item => item.name);
+  const type = data.map(item => item.objectType);
+  console.log("Names:",  names);
+  console.log("type:",  type);
+
+  const tokenizedNames = names.map(name => name.split(' '));
+  console.log(tokenizedNames)
+  
+  names.forEach((name, index) => {
+    const div = document.createElement('div');
+    let lowerCaseName = name.toLowerCase();
+    let tokens = lowerCaseName.split(' ');
+    console.log('this is my tokens', tokens);
+    div.style.fontSize = '0.5em';
+    subContainer.appendChild(div);
+
+    if ((index + 1) % 1 === 0) {
+    fragment.appendChild(subContainer);
+    subContainer = document.createElement('div');
     subContainer.className = 'sub-container';
-    
-    const tokenizedNames = names.map(name => name.split(' '));
-    console.log(tokenizedNames)
+    }
 
-    names.forEach((name, index) => {
-      const div = document.createElement('div');
-      let lowerCaseName = name.toLowerCase();
-      let tokens = lowerCaseName.split(' '); // Define tokens
-      console.log('this is my tokens', tokens);
-      let lowerCaseType = type[index].toLowerCase();
-      div.textContent = `${lowerCaseName} (${lowerCaseType})`;
-      div.style.fontSize = '0.9em';
-      subContainer.appendChild(div);
-
-      // Every 10 divs, create a new sub-container
-      if ((index + 1) % 1 === 0) {
-      fragment.appendChild(subContainer);
-      subContainer = document.createElement('div');
-      subContainer.className = 'sub-container';
+    tokens.forEach(token => { 
+      const span = document.createElement('span');
+      if (historical_context.some(w => token.toLowerCase() === w.toLowerCase())) {
+        span.textContent = token;
+        span.style.background = `${historyColor}`;
+        span.classList.add('historical');
+      } else if (politics.some(w => token.toLowerCase() === w.toLowerCase())) {
+        span.textContent = token;
+        span.style.background = `${politicColor}`;
+        span.classList.add('political');
+      } else if (social_and_cultural_ideologies.some(w => token.toLowerCase() === w.toLowerCase())) {
+        span.textContent = token;
+        span.style.background = 'green';
+        span.classList.add('social');
+      } else if (gender.some(w => token.toLowerCase() === w.toLowerCase())) {
+        span.textContent = token;
+        span.style.background = 'yellow';
+        span.classList.add('gender');
+      } else {
+        span.textContent = token;
+        span.classList.add('notHighlight');
       }
+      div.appendChild(span);
+      div.appendChild(document.createTextNode(' ')); // Add space between words
+    });;
+  }); 
 
-      tokens.forEach(token => {
-        const span = document.createElement('span');
-        if (historical_context.some(w => token.toLowerCase() === w.toLowerCase())) {
-          span.textContent = token;
-          span.style.background = 'red';
-          span.classList.add('historical');
-        } else if (politics.some(w => token.toLowerCase() === w.toLowerCase())) {
-          span.textContent = token;
-          span.style.background = 'blue';
-          span.classList.add('political');
-        } else if (social_and_cultural_ideologies.some(w => token.toLowerCase() === w.toLowerCase())) {
-          span.textContent = token;
-          span.style.background = 'green';
-          span.classList.add('social');
-        } else if (gender.some(w => token.toLowerCase() === w.toLowerCase())) {
-          span.textContent = token;
-          span.style.background = 'yellow';
-          span.classList.add('gender');
+
+  //btns container structure
+  let btnContainer = app.append('div').attr('class','btns')
+  let btns = document.createElement('div')
+  btns.id = 'butnContainer'
+  btnContainer.node().appendChild(btns);
+
+  function createButton(category, color, className) {
+    const button = document.createElement('button');
+    button.textContent = `${category}`;
+    button.style.backgroundColor = color;
+    button.style.border = 'none'; 
+    button.style.padding = '0.5em';
+   
+    button.addEventListener('click', () => {
+      const spans = document.querySelectorAll(`.${className}`);
+      spans.forEach(span => {
+        if (span.style.backgroundColor === color) {
+          span.style.backgroundColor = '';
         } else {
-          span.textContent = token;
+          span.style.backgroundColor = color;
         }
-        div.appendChild(span);
-        div.appendChild(document.createTextNode(' ')); // Add space between words
-      });;
-    }); 
-
-    function createButton(category, color, className) {
-      const button = document.createElement('button');
-      button.textContent = `${category}`;
-      button.style.backgroundColor = color;
-      button.style.border = 'none'; // Remove the border
-      button.style.padding = '0.5em'; // Remove the border
-      button.addEventListener('click', () => {
-        const spans = document.querySelectorAll(`.${className}`);
-        spans.forEach(span => {
-          if (span.style.backgroundColor === color) {
-            span.style.backgroundColor = '';
-          } else {
-            span.style.backgroundColor = color;
-          }
-        });
-        const rect = document.querySelector(`.rect.${className}`);
-        if (rect) rect.classList.toggle('highlight');
       });
-      return button;
-    }
+      const rect = document.querySelector(`${className}`);
+      if (rect) rect.classList.toggle('highlight');
+    });
+    return button;
+  }
 
-    const buttonContainerElement = document.getElementById('butnContainer');
-    if (buttonContainerElement) {
-      const buttonContainer = d3.select(buttonContainerElement).append('div').attr('class', 'button-container');
-      buttonContainer.node().appendChild(createButton('Politics', 'blue', 'political'));
-      buttonContainer.node().appendChild(createButton('Gender', 'yellow', 'gender'));
-      buttonContainer.node().appendChild(createButton('Social', 'green', 'social'));
-      buttonContainer.node().appendChild(createButton('Historical', 'red', 'historical'));
-    } else {
-      console.error('Element with ID "butnContainer" not found.');
-    }
+  if (btns) {
+    const buttonContainer = d3.select(btns).append('div').attr('class', 'button-container');
+    buttonContainer.node().appendChild(createButton('Politics', `${politicColor}`, 'political'));
+    buttonContainer.node().appendChild(createButton('Gender', 'yellow', 'gender'));
+    buttonContainer.node().appendChild(createButton('Social', 'green', 'social'));
+    buttonContainer.node().appendChild(createButton('Historical', `${historyColor}`, 'historical'));
+  } else {
+    console.error('Element with ID "butnContainer" not found.');
+  }
 
   let historicalCount = 0;
   let politicalCount = 0;
   let socialCount = 0;
   let genderCount = 0;
 
-function countWords(data) {
-  data.forEach(item => {
-    const tokens = item.name.toLowerCase().split(' ');
-    tokens.forEach(token => {
-      if (historical_context.some(w => token.toLowerCase() === w.toLowerCase())) {
-        historicalCount++;
-      }
-      if (social_and_cultural_ideologies.some(w => token.toLowerCase() === w.toLowerCase())) {
-        socialCount++;
-      }
-      if (politics.some(w => token.toLowerCase() === w.toLowerCase())) {
-        politicalCount++;
-      }
-      if (gender.some(w => token.toLowerCase() === w.toLowerCase())) {
-        genderCount++;
-      }
+  function countWords(data) {
+    data.forEach(item => {
+      const tokens = item.name.toLowerCase().split(' ');
+      tokens.forEach(token => {
+        if (historical_context.some(w => token.toLowerCase() === w.toLowerCase())) {
+          historicalCount++;
+        }
+        if (social_and_cultural_ideologies.some(w => token.toLowerCase() === w.toLowerCase())) {
+          socialCount++;
+        }
+        if (politics.some(w => token.toLowerCase() === w.toLowerCase())) {
+          politicalCount++;
+        }
+        if (gender.some(w => token.toLowerCase() === w.toLowerCase())) {
+          genderCount++;
+        }
+      });
     });
-  });
-
-  // Display the counts
-  const app = d3.select('#appContainer');
-  const chartContainer = d3.select('#chartContainer').attr('class', 'count-display');;
   
+    //secondary visualization div structure
+    const secVizContainer = app.append('div').attr('class','sec-viz-container')
+    const chartContainer = secVizContainer.append('div').attr('id','chartContainer').attr('class','chartContainer')
+    secVizContainer.node().appendChild(chartContainer.node());
 
-  // Create a rect for historical count
-  chartContainer.append('div')
-    .attr('class', 'rect-container')
-    .append('div')
-    .attr('class', 'rect historical')
-    .style('width', `${historicalCount * 10}px`)
-    .style('background-color', 'red')
-    .text(`Historical Count: ${historicalCount}`);
+    // Bar for each group
+    const totalCount = historicalCount + politicalCount + socialCount + genderCount;
+    const historicalWidth = (historicalCount / totalCount) * 100;
+    const politicalWidth = (politicalCount / totalCount) * 100;
+    const socialWidth = (socialCount / totalCount) * 100;
+    const genderWidth = (genderCount / totalCount) * 100;
 
-  // Create a rect for political count
-  chartContainer.append('div')
-    .attr('class', 'rect-container')
-    .append('div')
-    .attr('class', 'rect political')
-    .style('width', `${politicalCount * 10}px`)
-    .style('background-color', 'blue')
-    .text(`Political Count: ${politicalCount}`);
-    
-    chartContainer.append('div')
-    .attr('class', 'rect-container')
-    .append('div')
-    .attr('class', 'rect social')
-    .style('width', `${socialCount * 10}px`)
-    .style('background-color', 'green')
-    .text(`Social Count: ${socialCount}`);
-  
-    chartContainer.append('div')
-    .attr('class', 'rect-container')
-    .append('div')
-    .attr('class', 'rect gender')
-    .style('width', `${genderCount * 10}px`)
-    .style('background-color', 'yellow')
-    .text(`Gender Count: ${genderCount}`);
-}
+    const stackedBar = chartContainer.append('div')
+      .attr('class', 'stacked-bar')
+      .style('display', 'flex')
+      .style('width', '100%');
 
+    stackedBar.append('div')
+      .attr('class', 'rect-historical')
+      .style('width', `${historicalWidth}%`)
+      .style('background-color', `${historyColor}`)
+      .style('padding', '0.2em');
+
+    stackedBar.append('div')
+      .attr('class', 'rect-political')
+      .style('width', `${politicalWidth}%`)
+      .style('background-color',  `${politicColor}`)
+      .style('padding', '0.2em');
+
+    stackedBar.append('div')
+      .attr('class', 'rect-social')
+      .style('width', `${socialWidth}%`)
+      .style('background-color', 'green')
+      .style('padding', '0.2em');
+
+    stackedBar.append('div')
+      .attr('class', 'rect-gender')
+      .style('width', `${genderWidth}%`)
+      .style('background-color', 'yellow')
+      .style('padding', '0.2em');
+  }
   countWords(data);
 
-    // Append the last sub-container if it has any children
-    if (subContainer.children.length > 0) {
-      fragment.appendChild(subContainer);
-    }
-    mainContainer.append(() => fragment);
-  
-    // You can call processRita here if you want to process the fetched data
-    // processRita(data.someTextProperty);
-  })
-  .catch(error => {
-    hideLoading();
-    console.error('Error:', error);
-  });
-  
-  // Function to create a toggle button
+  // Append the last sub-container if it has any children
+  if (subContainer.children.length > 0) {
+    fragment.appendChild(subContainer);
+  }
+  mainContainer.append(() => fragment);
 
+}).catch(error => {
+  hideLoading();
+  console.error('Error:', error);
+});
 
-  // Call the function to create the toggle button
-  
